a42fbb6f5d89823f853d8036231ce475642774a9
Guira <guira@Guiras-MacBook-Pro.local>
2020-11-07 09:37:48 +0100

    Updates

---
+++ precache-manifest.e44526b35e5b6fa16491fd7837b847c0.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f3e4c02de94585262f2d8f7b25b6c7",
+    "url": "/guillermo-Conde-Portfolio/index.html"
+  },
+  {
+    "revision": "63ba1d869f75f3de2bc5",
+    "url": "/guillermo-Conde-Portfolio/static/css/2.e5d0dddf.chunk.css"
+  },
+  {
+    "revision": "c2f45fc472a1c28025d5",
+    "url": "/guillermo-Conde-Portfolio/static/css/main.c0203237.chunk.css"
+  },
+  {
+    "revision": "63ba1d869f75f3de2bc5",
+    "url": "/guillermo-Conde-Portfolio/static/js/2.975878f5.chunk.js"
+  },
+  {
+    "revision": "4a7805e759e8c5224420cd0ecc7191ae",
+    "url": "/guillermo-Conde-Portfolio/static/js/2.975878f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f45fc472a1c28025d5",
+    "url": "/guillermo-Conde-Portfolio/static/js/main.46ec9d87.chunk.js"
+  },
+  {
+    "revision": "d7869dd3d85852e613a3",
+    "url": "/guillermo-Conde-Portfolio/static/js/runtime-main.e6acd594.js"
+  },
+  {
+    "revision": "2cef04254dcb2dd4ff45c094e25facd3",
+    "url": "/guillermo-Conde-Portfolio/static/media/42.2cef0425.png"
+  },
+  {
+    "revision": "f31ca048faafe818d62e12509efb1e21",
+    "url": "/guillermo-Conde-Portfolio/static/media/buddy.f31ca048.png"
+  },
+  {
+    "revision": "a58f4a9d0385da8af678042aa717f1f0",
+    "url": "/guillermo-Conde-Portfolio/static/media/buddyvideo.a58f4a9d.gif"
+  },
+  {
+    "revision": "37d6994d269904a0dca99c6af9579048",
+    "url": "/guillermo-Conde-Portfolio/static/media/code_animated.37d6994d.svg"
+  },
+  {
+    "revision": "04d96c3e0a5668b3d3ea585142c13ce9",
+    "url": "/guillermo-Conde-Portfolio/static/media/cub3d.04d96c3e.gif"
+  },
+  {
+    "revision": "0f29f0d86ccd252fd36365885eee9e97",
+    "url": "/guillermo-Conde-Portfolio/static/media/cub3d.0f29f0d8.png"
+  },
+  {
+    "revision": "bf1977ab67c94f1063e9f61fe3a5f8bc",
+    "url": "/guillermo-Conde-Portfolio/static/media/foto2.bf1977ab.png"
+  },
+  {
+    "revision": "037b1eb7208a995a0a69d6460ad47c48",
+    "url": "/guillermo-Conde-Portfolio/static/media/ironhack.037b1eb7.png"
+  },
+  {
+    "revision": "121357fa2fc7de3e65c2347b4534fba6",
+    "url": "/guillermo-Conde-Portfolio/static/media/jungleninja.121357fa.png"
+  },
+  {
+    "revision": "0b63d08aa5531e0a33ddc743bdad5a41",
+    "url": "/guillermo-Conde-Portfolio/static/media/logo-femxa.0b63d08a.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/guillermo-Conde-Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "70564d41722041d0fb88e338845baa64",
+    "url": "/guillermo-Conde-Portfolio/static/media/motiv.70564d41.png"
+  },
+  {
+    "revision": "8cf424f9d4838b08440ddc3414707128",
+    "url": "/guillermo-Conde-Portfolio/static/media/motivvideo.8cf424f9.gif"
+  },
+  {
+    "revision": "714618e30bc973453e253513ffb214a5",
+    "url": "/guillermo-Conde-Portfolio/static/media/navlogo.714618e3.png"
+  },
+  {
+    "revision": "7273e319c24e109aef24d260252bdc50",
+    "url": "/guillermo-Conde-Portfolio/static/media/ninjavideo.7273e319.gif"
+  },
+  {
+    "revision": "70288eb5594010bbdfd5175f21cfdf41",
+    "url": "/guillermo-Conde-Portfolio/static/media/portfolio.70288eb5.png"
+  },
+  {
+    "revision": "81fa27393f50a436bc4e4320bf1eeb12",
+    "url": "/guillermo-Conde-Portfolio/static/media/printf.81fa2739.png"
+  },
+  {
+    "revision": "7627bc820cce40098e897ca98413d895",
+    "url": "/guillermo-Conde-Portfolio/static/media/three-dots.7627bc82.svg"
+  },
+  {
+    "revision": "13078d3724fd881a36204970293c49d1",
+    "url": "/guillermo-Conde-Portfolio/static/media/udemy.13078d37.png"
+  }
+]);
